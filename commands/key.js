2d66778d01ddd3bd047638d72ba9e2fd33ea50c9
Ahmad Ali <aallii300300@gmail.com>
2020-04-06 13:15:46 +0100

    set key

--- commands/key.js
+++ commands/key.js
@@ -1,13 +1,62 @@
 /**
- * this file contains sub-commands' action functions for the main command (key).
+ * this file contains sub-commands' action functions for the main command (key) inside the 
+ *  file bin/schecker-key.js .
  */
 
+ const KeyManager = require('../lib/KeyManager.js');
+ const inquirer = require('inquirer');
+ const colors = require('colors');
+ const isRequired = require('../utils/validation').isRequired;
+
 const key = {
-    set(){
-        console.log('set !!');
+    async set(){
+        /**
+         * instanciate a new KeyManager
+         */
+        const keyManager = new KeyManager();
+
+        /** prompting the user for API key using inquirer, we need to wait until the user give the answer
+        */ 
+       const input = await inquirer
+                            /**
+                             * provide the inquirer with questions to ask the user.
+                             * the questions should be an array, each question is an object 
+                              */    
+                            .prompt([{
+                                type: 'input',
+                                name: 'key', 
+                                message: 'Enter API key'.green +' https://nomics.com/',
+                                validate: isRequired
+                            }])
+                            
+        /**
+         *  extracting the key from input and store it in our configstore
+         */
+        const key = keyManager.setKey(input.key);
+
+        /**
+         * success message
+         */
+        if(key){
+            console.log('API Key Set'.blue);
+        }
+
     },
     show(){
-        console.log('show !!');
+        /**
+         * 
+         */
+        try{
+            /**
+             * instanciate a new KeyManager
+             */
+            const keyManager = new KeyManager();
+            /**
+             * extract saved key  
+             */
+        } catch (err){
+
+        }
     },
     remove(){
         console.log('remove !!');
